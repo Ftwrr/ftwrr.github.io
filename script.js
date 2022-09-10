2496fdfc6feb7af4ea168b3771aa5522c79dcc77
Fatur <76021496+Ftwrr@users.noreply.github.com>
2022-09-10 20:41:39 +0800

    external css and javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeImage() {
+  if (
+    document.getElementById("johan").src ==
+    "https://i.pinimg.com/originals/36/d3/a0/36d3a0fc51c40d18b0fb731ff36c80a5.gif"
+  ) {
+    document.getElementById("johan").src =
+      "https://i.ytimg.com/vi/xzy_0m7jwRw/maxresdefault.jpg";
+  } else {
+    document.getElementById("johan").src =
+      "https://i.pinimg.com/originals/36/d3/a0/36d3a0fc51c40d18b0fb731ff36c80a5.gif";
+  }
+}
