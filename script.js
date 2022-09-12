41862bf260d7c5378b47c78667b550ae33060f9e
Fatur <76021496+Ftwrr@users.noreply.github.com>
2022-09-12 21:48:55 +0800

    add shuffle song and alert onclick johan

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-function changeImage() {
-  if (
-    document.getElementById("johan").src ==
-    "https://i.pinimg.com/originals/36/d3/a0/36d3a0fc51c40d18b0fb731ff36c80a5.gif"
-  ) {
-    document.getElementById("johan").src =
-      "https://i.ytimg.com/vi/xzy_0m7jwRw/maxresdefault.jpg";
-  } else {
-    document.getElementById("johan").src =
-      "https://i.pinimg.com/originals/36/d3/a0/36d3a0fc51c40d18b0fb731ff36c80a5.gif";
-  }
+function Hello() {
+   alert("Hello world");
+}
+function shuffleMusic() {
+   var Music = ["hotel.mp3", "iwby.mp3", "505.mp3", "ewa.mp3", "diwk.mp3", "theshade.mp3", "amazing.mp3", "l.mp3"]
+  document.getElementById("music").src = 'media/' + random_item(Music)
+}
+function random_item(items) {
+   return items[Math.floor(Math.random()*items.length)];
 }
