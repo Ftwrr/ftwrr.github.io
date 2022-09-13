463df17ee65febd926dc1abf5e227c29f72b41d7
Fatur <76021496+Ftwrr@users.noreply.github.com>
2022-09-13 19:44:41 +0800

    pake match dan replace

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function Hello() {
-   alert("Hello world");
+function changeImage(img) {
+   if (img.src.match("johan.gif")) {
+      img.src = img.src.replace("johan.gif", "nerd.jpg")
+   } else {
+      img.src = img.src.replace("nerd.jpg", "johan.gif")
+   }
 }
 function shuffleMusic() {
    var Music = ["hotel.mp3", "iwby.mp3", "505.mp3", "ewa.mp3", "diwk.mp3", "theshade.mp3", "amazing.mp3", "l.mp3"]
